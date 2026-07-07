c5d775c63b1bf7106c66e41aac0c803bfd93bad7
utsav gangadiya <utsavgangadiya886@gmail.com>
2026-07-07 15:05:39 +0530

    practice of string

---
+++ string/basic.py
@@ -0,0 +1,6 @@
+str1="Hello\n"
+str2='Have a nice day!\n'
+str3="""Happy to connect with you give star to repo"""
+
+print(str1,str2,str3)
+
---
+++ string/practice.py
@@ -0,0 +1,28 @@
+# Take input from the user and print length 
+
+# str=input("Enter your name :")
+
+# print(len(str))
+
+# find occurrence of @ in a string 
+
+# string = "$ Hello my name$ is utsav"
+# print(string.count("$"))
+
+# Reverse Words in a Given String in Python
+
+s = "Python is fun"
+res = (s.split()[::-1])
+print(type(res))
+res = ' '.join(s.split()[::-1])
+print(res)
+
+# Swap commas and dots in a String
+
+t = str.maketrans(',.', '.,')
+str = "89, 6392, 673.280"
+
+
+res = str.translate(t)
+print(res)
+
